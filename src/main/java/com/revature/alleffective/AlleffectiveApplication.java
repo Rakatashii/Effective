34f63f73c1e8abfaadb20202e42ac4afb962e325
vn0du1v <vn0du1v@homeoffice.wal-mart.com>
2019-06-06 19:14:24 -0500

    init

---
+++ src/main/java/com/revature/alleffective/AlleffectiveApplication.java
@@ -0,0 +1,13 @@
+package com.revature.alleffective;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlleffectiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlleffectiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revature/alleffective/AlleffectiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.alleffective;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlleffectiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
