5c2491a91cee5d0baccda098f1378118567834d6
vn0du1v <vn0du1v@homeoffice.wal-mart.com>
2019-06-06 20:25:39 -0500

    basic multimodule

---
+++ core/src/main/java/com/revature/core/App.java
@@ -0,0 +1,9 @@
+package com.revature.core;
+
+public class App {
+
+	public static void main(String[] args) {
+
+	}
+
+}
--- z-ten/src/main/java/com/revature/effective/ten/OneThroughTen.java
+++ z-ten/src/main/java/com/revature/effective/ten/OneThroughTen.java
@@ -1,13 +1,11 @@
-package com.revature.alleffective;
+package com.revature.effective.ten;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AlleffectiveApplication {
-
+public class OneThroughTen {
 	public static void main(String[] args) {
-		SpringApplication.run(AlleffectiveApplication.class, args);
+		SpringApplication.run(OneThroughTen.class, args);
 	}
-
 }
